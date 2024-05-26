02d1c5a5fce41779f54591dc8ef2eb9f143bde0b
blister-pack <andrecrujo@hotmail.com>
2024-05-26 14:16:48 +0200

    reverse string func not working

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,19 +1,18 @@
 const reverseString = function(toReverse) {
     let iterator = toReverse.length;
+    console.log(iterator);
+    let reversedString = "";
 
-    for (iterator; iterator <= 0; iterator--) {
+    for (iterator; iterator < 0; iterator--) {
         let letterToAdd = toReverse[iterator - 1];
-        
-        
-        
+        console.log(letterToAdd);
+        reversedString += letterToAdd;
+        console.log(reverseString);
     }
+    return reversedString;
 };
+
+reverseString("Hello!")
 
 // Do not edit below this line
 module.exports = reverseString;
-
-let bla = "tuturara"
-
-let oi = bla.charAt
-
-console.log(oi);
