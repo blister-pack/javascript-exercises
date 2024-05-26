77ff19d554d3d26c5ac83ad875b29a199b129d00
André Crujo <119133995+blister-pack@users.noreply.github.com>
2024-05-26 14:48:11 +0200

    Update reverseString.js found error

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -5,6 +5,7 @@ const reverseString = function(toReverse) {
 
     for (iterator; iterator < 0; iterator--) {
         let letterToAdd = toReverse[iterator - 1];
+// for loop never executes because iterator is never smaller than 0
         console.log(letterToAdd);
         reversedString += letterToAdd;
         console.log(reverseString);
