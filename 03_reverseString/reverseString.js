ab868c7804853e8fb509941626e8287f487eeb63
blister-pack <andrecrujo@hotmail.com>
2024-05-26 22:52:56 +0200

    reverse string complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,10 @@
 const reverseString = function(toReverse) {
     let iterator = toReverse.length;
-    console.log(iterator);
     let reversedString = "";
 
-    for (iterator; iterator < 0; iterator--) {
+    for (iterator; iterator > 0; iterator--) {
         let letterToAdd = toReverse[iterator - 1];
-// for loop never executes because iterator is never smaller than 0
-        console.log(letterToAdd);
         reversedString += letterToAdd;
-        console.log(reverseString);
     }
     return reversedString;
 };
