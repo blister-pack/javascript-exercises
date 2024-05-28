27b0a03de9d3e4939c223a541c2922d9f55b01b7
blister-pack <andrecrujo@hotmail.com>
2024-05-28 17:42:15 +0200

    sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(rangeStart, rangeEnd) {
 
+
+    function checkConditionsToStart(rangeStart, rangeEnd) {
+        const conditions = [
+            // does not take in negative numbers
+            (rangeStart) => rangeStart < 0,
+            (rangeEnd) => rangeEnd < 0,
+            
+            // does not take in non-numbers
+            (rangeStart) => isNaN(rangeStart),
+            (rangeEnd) => isNaN(rangeEnd),
+        ];
+
+        if (conditions.includes(true)) {
+            return true;
+            // then the main func should return "ERROR"
+        }
+        return false;
+        // then the sum can be calculated
+
+
+    }
+    
 };
 
 // Do not edit below this line
