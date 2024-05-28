0e9ceaea3dd86b842d8d26facfbcae4f095da51f
blister-pack <andrecrujo@hotmail.com>
2024-05-28 23:14:05 +0200

    works with non numerical values, need to fix

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,17 +3,19 @@ const sumAll = function(rangeStart, rangeEnd) {
     function checkConditionsToStart() {
         const conditions = [
             // does not take in negative numbers
-            (negativeStart) => rangeStart < 0,
-            (negativeEnd) => rangeEnd < 0,
+            () => rangeStart < 0,
+            () => rangeEnd < 0,
             
             // does not take in non-numbers
-            (isNumberStart) => isNaN(rangeStart),
-            (isNumberEnd) => isNaN(rangeEnd),
+            () => isNaN(rangeStart),
+            () => isNaN(rangeEnd),
         ];
 
-        if (conditions.includes(true)) {
-            return true;
-            // then the main func should return "ERROR"
+        for (const condition of conditions) {
+            if (condition()) {
+                return true;
+                // then the main func should return "ERROR"
+        }
         }
         return false;
         // then the sum can be calculated
@@ -35,7 +37,7 @@ const sumAll = function(rangeStart, rangeEnd) {
     
 };
 
-sumAll(1, 4)
+sumAll(1, -4)
 
 // Do not edit below this line
 module.exports = sumAll;
