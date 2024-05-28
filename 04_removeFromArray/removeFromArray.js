a55d3b811e695f841e68593b6eb5d179c266566a
blister-pack <andrecrujo@hotmail.com>
2024-05-28 16:19:44 +0200

    2nd test doesn't pass - debugging

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,23 +4,18 @@ const removeFromArray = function (arrayToModify, ...valuesToRemove) {
     // every func
     for (const huntedValue of valuesToRemove) {
         while (arrayToModify.every(number => number != huntedValue) === false) {
-            let indexNumToRemove = arrayToModify.findIndex(value => value === huntedValue);
+            // let indexNumToRemove = arrayToModify.findIndex(value => value === huntedValue);
+            let indexNumToRemove = arrayToModify.indexOf(huntedValue);
+            // this function is more appropriate for this use
             arrayToModify.splice(indexNumToRemove, 1);
         }
     
     return arrayToModify
-    
-        // for (const arrayNumToCompare of arrayToModify) {
-        //     if (huntedValue === arrayNumToCompare) {
-
-        //     }
-        // }
-        
     }
 
 };
 
-// removeFromArray([1, 2, 3, 4], 3)
+removeFromArray([1, 2, 3, 4], 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
