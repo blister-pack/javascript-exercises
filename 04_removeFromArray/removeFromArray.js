e8f283b3fd7319b0b5a1e97a66e154a2cd6e30f3
blister-pack <andrecrujo@hotmail.com>
2024-05-28 14:16:43 +0200

    logic done but func not working, fixing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,26 @@
 const removeFromArray = function (arrayToModify, ...valuesToRemove) {
     // splice func
+    // findIndex func
+    // every func
     for (const huntedValue of valuesToRemove) {
-        for (const arrayNumToCompare of arrayToModify) {
-            
+        while (arrayToModify.every(number => number != huntedValue) === false) {
+            let indexNumToRemove = arrayToModify.findIndex(huntedValue);
+            arrayToModify.splice(indexNumToRemove, 1);
         }
+    
+    return arrayToModify
+    
+        // for (const arrayNumToCompare of arrayToModify) {
+        //     if (huntedValue === arrayNumToCompare) {
+
+        //     }
+        // }
         
     }
 
 };
 
-function numberInArray(num) {
-    // idk
-};
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
