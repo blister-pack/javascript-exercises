eb0dbd72f4e656b768e642c8aba8f3c7d9d0fa7d
blister-pack <andrecrujo@hotmail.com>
2024-05-29 20:55:42 +0200

    palindromes exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,35 @@
 const palindromes = function (paliCheck) {
     let checker = "";
     let invertedChecker;
+    paliCheck = paliCheck.toLowerCase();
 
     for (const letter of paliCheck) {
         if (isLetter(letter)) {
-            checker += letter
+            checker += letter;
             // we have a string with only letters
         }
     }
 
-    invertedChecker = checker.reverse()
+    invertedChecker = checker;
+    invertedChecker = invertedChecker.split("").reverse().join("");
+    console.log(invertedChecker);
+
+
+    if (invertedChecker === checker) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
 const isLetter = function (letter) {
-    let letterChecker = /^[a-zA-Z]+$/;
-    return letterChecker.test(letter)
+    let letterChecker = /^[a-zA-Z0-9]+$/;
+    return letterChecker.test(letter);
 };
+
+// console.log(palindromes("abba"));
+// palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
