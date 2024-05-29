c78d7bc48e0ca8e050f539edd444e07db7ef3b81
blister-pack <andrecrujo@hotmail.com>
2024-05-29 14:13:05 +0200

    tempConversion exercise finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
 const convertToCelsius = function (fahrenToConvert) {
   let convertedTemp = (fahrenToConvert - 32) * (5 / 9);
-  return parseFloat(convertedTemp.toFixed(2));
+  return parseFloat(convertedTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusToConvert) {
+  let convertedTemp = (celsiusToConvert * (9 / 5) + 32);
+  return parseFloat(convertedTemp.toFixed(1));
 };
+
+convertToCelsius(100);
 
 // Do not edit below this line
 module.exports = {
