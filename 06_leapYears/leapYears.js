cadd7778a79471ed477feab0581380ff91880476
blister-pack <andrecrujo@hotmail.com>
2024-05-29 11:48:01 +0200

    leapyear exercise complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function(yearToCheck) {
-
+    if (yearToCheck % 4 === 0 && yearToCheck % 100 !== 0) {
+        return true
+    } else if (yearToCheck % 100 === 0 && yearToCheck % 400 === 0) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
