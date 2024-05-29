cfeb3a70e1f1f2bb8f768195c76ee14f9463d1af
blister-pack <andrecrujo@hotmail.com>
2024-05-29 16:11:41 +0200

    calculator complete; palindrome working on it

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (paliCheck) {
+    let checker = "";
+    let invertedChecker;
 
+    for (const letter of paliCheck) {
+        if (isLetter(letter)) {
+            checker += letter
+            // we have a string with only letters
+        }
+    }
+
+    invertedChecker = checker.reverse()
+
+};
+
+const isLetter = function (letter) {
+    let letterChecker = /^[a-zA-Z]+$/;
+    return letterChecker.test(letter)
 };
 
 // Do not edit below this line
