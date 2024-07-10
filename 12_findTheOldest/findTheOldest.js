3fc0f16af3e23fbe810a4b6b8cb8732b80c38b88
blister-pack <andrecrujo@hotmail.com>
2024-07-10 14:58:33 +0200

    findTheOldest exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    // find function that returns today's date
+    let oldestPerson;
+    
+    function getAge(person) {
+        let thisYear = new Date().getFullYear()
+        if (person?.yearOfBirth && person?.yearOfDeath) {
+            return (person.yearOfDeath - person.yearOfBirth);
+        } else if (person?.yearOfBirth) {
+            return (thisYear - person.yearOfBirth);
+        }
+        return 0;
+    }
 
+    people.map((person) => {
+        if (getAge(person) > getAge(oldestPerson)) {
+            oldestPerson = person;
+        }
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
